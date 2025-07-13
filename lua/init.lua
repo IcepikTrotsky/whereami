a4d3a3c347130e6f0ede0f2c00ce11c7f8120199
Luke Boyes <luke.boyes@gmail.com>
2025-07-13 21:41:26 +1200

    feat: show function names instead of node types

--- lua/init.lua
+++ lua/init.lua
@@ -1,25 +1,40 @@
----TODO: Onlyf get ancestors that match some properties, like being a function
+local query_string = [[
+		(function_declaration
+	  name: (_) @ancestor.name
+	) @ancestor
+	]]
+local query = vim.treesitter.query.parse("lua", query_string)
+
 ---@param node TSNode
+---@return table<integer, TSNode[]>
 local function get_ancestors(node)
 	local ancestor = node:tree():root() ---@type TSNode?
 	local ancestors = {}
 
 	while ancestor do
-		table.insert(ancestors, ancestor)
+		for _, match in query:iter_matches(ancestor, 0, nil, nil, { max_start_depth = 0 }) do
+			table.insert(ancestors, match)
+		end
 		ancestor = ancestor:child_with_descendant(node)
 	end
 
 	return ancestors
 end
 
----TODO: Show function names etc rather than node types
 ---TODO: Show linecount of immediate parent
 ---@param node TSNode
 local function build_node_path(node)
 	local node_path = ""
 
 	for _, ancestor in pairs(get_ancestors(node)) do
-		node_path = node_path .. ancestor:type() .. " | "
+		for cap, nodes in pairs(ancestor) do
+			-- TODO: How do we ensure that this function has access to
+			-- this query without it being a global variable?
+			if query.captures[cap] == "ancestor.name" then
+				-- TODO: We're assuming that there will only ever be one node here
+				node_path = node_path .. vim.treesitter.get_node_text(nodes[1], 0) .. " | "
+			end
+		end
 	end
 
 	return node_path
